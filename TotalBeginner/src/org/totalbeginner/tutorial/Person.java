aba13d2456e79154040e64fca35cc5d1061bdcbd
sehmig <sehmig@SL510>
2016-05-11 22:05:05 +0200

    Test

---
+++ TotalBeginner/src/org/totalbeginner/tutorial/Person.java
@@ -0,0 +1,26 @@
+package org.totalbeginner.tutorial;
+
+public class Person {
+	//fields
+	private String name; //name of the person
+	private int maximumBooks; //max number ofbooks zu check out
+	// constructors
+	public Person() {
+		name = "unknown name";
+		maximumBooks = 3;	
+	}
+	
+	// methods
+	public String getName() {
+		return name;
+	}
+	public void setName(String anyName) {
+		name = anyName;
+	}
+	public int getMaximumBooks() {
+		return maximumBooks;
+	}
+	public void setMaximumBooks(int maximumBooks) {
+		this.maximumBooks = maximumBooks;
+	}
+}
